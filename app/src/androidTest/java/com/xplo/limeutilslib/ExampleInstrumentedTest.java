4adfc0a1abcc4c3ce18da27a942e569e49d4a559
Nasif.Ahmed <nasif.ahmed@bongobd.com>
2019-09-09 17:39:24 +0600

    update dependency

--- app/src/androidTest/java/com/xplo/limeutilslib/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/xplo/limeutilslib/ExampleInstrumentedTest.java
@@ -1,8 +1,11 @@
 package com.xplo.limeutilslib;
 
+
+
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
