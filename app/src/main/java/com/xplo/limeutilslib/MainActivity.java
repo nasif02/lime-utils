1159fdb4e5e577feb4a51dcb43f3f9120b896e33
xplo <nasif.002@gmail.com>
2019-10-16 00:53:32 +0600

    testing DbHelper class

--- app/src/main/java/com/xplo/limeutilslib/MainActivity.java
+++ app/src/main/java/com/xplo/limeutilslib/MainActivity.java
@@ -2,6 +2,7 @@ package com.xplo.limeutilslib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    DbHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         Contextor.getInstance().init(getApplicationContext());
 
-        boolean dbExist = DbHelper.getInstance("db.db").checkDataBase();
-        Log.d(TAG, "onCreate: " + dbExist);
+//        boolean dbExist = DbHelper.getInstance("db.db").isDbExist();
+//        Log.d(TAG, "onCreate: " + dbExist);
+//
 
+        db = new DbHelper(this,"db2.db");
 
+//        try {
+//            db.createDataBase();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            DbHelper.getInstance().createDataBase();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        db.openDataBase();
 
-        dbExist = DbHelper.getInstance("db.db").checkDataBase();
-        Log.d(TAG, "onCreate: " + dbExist);
+        db.createTableDemo();
 
+        getListItems();
+
+        insertItem();
 
         getListItems();
 
@@ -46,14 +54,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void insertItem() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("title", "title");
+        contentValues.put("body", "body");
+        contentValues.put("bookmark", 1);
+        contentValues.put("category", "aaa");
+
+        db.insertRow("demo", contentValues);
+    }
+
     private void getListItems() {
 
         List<String> listArray = new ArrayList<>();
 
-        String sql = null;
-        Cursor cursor;
+        String sql = "select * from demo";
 
-        sql = "select * from ";
+        Cursor cursor = db.runQueryRaw(sql,null);
+
+        Log.d(TAG, "getListItems: " + cursor.getCount());
+
 
     }
 }
